0935d7f9e323f645b18fd6ced946555f69a3dffa
Nick Winkelbauer <nwinkelb@uw.edu>
2016-07-31 17:04:04 -0700

    modified pop-up boxes and cleaned up ajax call format.

--- js/test.js
+++ js/test.js
@@ -8,37 +8,36 @@ L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/
     minZoom: 8,
 }).addTo(mymap);
 
-//add the geojson file
-var district_boundary = new L.geoJson();
-district_boundary.addTo(mymap);
-//district lines
+
+//add district lines to map from geojson file
 $.ajax({
 dataType: "json",
 url: "data/demodistrict.geojson",
-success: function(data) {
-    $(data.features).each(function(key, data) {
-        if(data.properties.districtdemo_freeredlunch){
-            district_boundary.addData(data);
-            district_boundary.setStyle(style);
-            district_boundary.bindPopup(getDistrictPopup(data));
-        }
-        //console.log(data)
-    });
+success: function(e) {
+    L.geoJson(e, {
+        filter: filterDistrict,
+        onEachFeature: onEachDistrict,
+        style: styleDistrict
+    }).addTo(mymap);
 }
 }).error(function() {});
 
-//school sites
-var school_sites = new L.geoJson();
-//school_sites.addTo(mymap);
+
+//add legend for cloropleth to map
+var legend = L.control({position: 'bottomright'});
+legend.onAdd = legendAdd;
+legend.addTo(mymap);
+
+
+//add school sites to map from cloropleth
 $.ajax({
 dataType: "json",
 url: "data/demoschool.geojson",
-success: function(data) {
-    $(data.features).each(function(key, data) {
-        if(data.geometry.coordinates[0] !== 0) {
-            school_sites.addData(data);
-        }
-        //console.log(data.geometry.coordinates[0])
+success: function(e) {
+    school_sites = L.geoJson(e, {
+        filter: filterSchool,
+        onEachFeature: onEachSchool
+        //style: styleSchool
     });
 }
 }).error(function() {});
@@ -61,18 +60,110 @@ mymap.on('zoomend', function (e) {
 });
 
 //color for cloropleth
-var getColor = chroma.scale(['#00FF00', '#FFFF00', '#FF0000']).domain([0, 25, 100]); 
-function style(feature) {
-    console.log(feature);
+function getColor(e) {
+    var colored = chroma.scale(['#00FF00', '#FFFF00', '#FF0000']).domain([0, 25, 100]);
+    return colored(e);
+} 
+
+//add color of cloropleth to districts
+function styleDistrict(feature) {
     return {
         fillColor: getColor(feature.properties.districtdemo_freeredlunch)
     };
 }
 
-//add legend for cloropleth
-var legend = L.control({position: 'bottomright'});
+//filter districts on whether they have info
+function filterDistrict(feature, layer) {
+    return feature.properties.districtdemo_freeredlunch;
+}
 
-legend.onAdd = function (map) {
+//filter school on whether they have coordinates
+function filterSchool(feature, layer) {
+    return (feature.geometry.coordinates[0] !== 0);
+}
+
+//for adding district pop-up
+function onEachDistrict(feature, layer) {
+    layer.bindPopup(getDistrictPopup(feature));
+}
+
+//for adding school pop-up
+function onEachSchool(feature, layer) {
+    layer.bindPopup(getSchoolPopup(feature));
+}
+
+//district pop up box
+function getDistrictPopup(data) {
+    graphHeight = 80;
+    asian = Math.round(parseInt(data.properties.districtdemo_PercentAsian) + parseInt(data.properties.districtdemo_PercentAsianPacificIslander));
+    hispanic = Math.round(data.properties.districtdemo_PercentHispanic);
+    white = Math.round(data.properties.districtdemo_PercentWhite);
+    african = Math.round(data.properties.districtdemo_PercentBlack);
+    return "<div class='popupbox'><table width='200px'>" + 
+                "<tr class='district-name row'><td colspan='4'>" + data.properties.NAME + "</td></tr>" +
+                "<tr class='lunch data row'><td class='percent'>" + Math.round(data.properties.districtdemo_freeredlunch) + "%" + "</td><td colspan='3' class='label'>% Free / reduced lunch</td></tr>" +
+                "<tr class='grad data row'><td class='percent'>" + Math.round(data.properties.districtdemo_GradRate) + "%" + "</td><td colspan='3' class='label'>Graduation rate</td></tr>" +
+                "<tr class='esl data row'><td class='percent'>" + Math.round(data.properties.districtdemo_ESL) + "%" + "</td><td colspan='3' class='label'>% ESL</td></tr>" +
+                "<tr class='graph row'>" + 
+                    "<td rowspan='5' class='asian'>" +  
+                        "<div class='bar' style='height:" + (asian/100 * graphHeight) + "px;'><p>" + asian + "%</p></div>" +
+                        "<p class='tag'>asian</p>" +
+                    "</td>" +
+                    "<td rowspan='5' class='hispanic'>" +  
+                        "<div class='bar' style='height:" + (hispanic/100 * graphHeight) + "px;'><p>" + hispanic + "%</p></div>" +
+                        "<p class='tag'>hispanic</p>" +
+                    "</td>" +
+                    "<td rowspan='5' class='white'>" +  
+                        "<div class='bar' style='height:" + (white/100 * graphHeight) + "px;'><p>" + white + "%</p></div>" +
+                        "<p class='tag'>white</p>" +
+                    "</td>" +
+                    "<td rowspan='5' class='african'>" +  
+                        "<div class='bar' style='height:" + (african/100 * graphHeight) + "px;'><p>" + african + "%</p></div>" +
+                        "<p class='tag'>african</p>" +
+                    "</td>" +
+                "</tr>" +
+                "</table></div>";
+}
+
+//district pop up box
+function getSchoolPopup(data) {
+    graphHeight = 75;
+    asian = Math.round(parseInt(data.properties.PercentAsian) + parseInt(data.properties.PercentAsianPacificIslander));
+    hispanic = Math.round(data.properties.PercentHispanic);
+    white = Math.round(data.properties.PercentWhite);
+    african = Math.round(data.properties.PercentBlack);
+    UWCand = data.properties.UWPrincipals + data.properties.UWSI + data.properties.UWTeachers;
+    //console.log(data);
+    popup = "<div class='popupbox'><table width='200px'>" + 
+                "<tr class='district-name row'><td colspan='4'>" + data.properties.School + "</td></tr>" +
+                "<tr class='lunch data row'><td class='percent'>" + Math.round(data.properties.PercentFreeorReducedPricedMeals) + "%" + "</td><td colspan='3' class='label'>% Free / reduced lunch</td></tr>";
+    popup += (data.properties.GraduationRate === 0) ? "" : "<tr class='grad data row'><td class='percent'>" + Math.round(data.properties.GraduationRate) + "%" + "</td><td colspan='3' class='label'>Graduation rate</td></tr>";
+                //"<tr class='esl data row'><td class='percent'>" + Math.round(data.properties.ESL) + "%" + "</td><td colspan='3' class='label'>% ESL</td></tr>" +
+    popup += (UWCand === 0) ? "" : "<tr class='grad data row'><td class='percent'>" + UWCand + "</td><td colspan='3' class='label'>UW leadership candidates</td></tr>";
+    popup +=    "<tr class='graph row'>" + 
+                    "<td rowspan='5' class='asian'>" +  
+                        "<div class='bar' style='height:" + (asian/100 * graphHeight) + "px;'><p>" + asian + "%</p></div>" +
+                        "<p class='tag'>asian</p>" +
+                    "</td>" +
+                    "<td rowspan='5' class='hispanic'>" +  
+                        "<div class='bar' style='height:" + (hispanic/100 * graphHeight) + "px;'><p>" + hispanic + "%</p></div>" +
+                        "<p class='tag'>hispanic</p>" +
+                    "</td>" +
+                    "<td rowspan='5' class='white'>" +  
+                        "<div class='bar' style='height:" + (white/100 * graphHeight) + "px;'><p>" + white + "%</p></div>" +
+                        "<p class='tag'>white</p>" +
+                    "</td>" +
+                    "<td rowspan='5' class='african'>" +  
+                        "<div class='bar' style='height:" + (african/100 * graphHeight) + "px;'><p>" + african + "%</p></div>" +
+                        "<p class='tag'>african</p>" +
+                    "</td>" +
+                "</tr>" +
+                "</table></div>";
+    return popup;
+}
+
+//create cloropleth scale
+function legendAdd(map) {
 
     var div = L.DomUtil.create('div', 'info legend'),
         grades = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
@@ -88,39 +179,5 @@ legend.onAdd = function (map) {
     div.innerHTML += '<br/>' + 'FREE / REDUCED LUNCH';
 
     return div;
-};
-
-legend.addTo(mymap);
-
-//district pop up box
-function getDistrictPopup(data) {
-    graphHeight = 100;
-    asian = Math.round(parseInt(data.properties.districtdemo_PercentAsian) + parseInt(data.properties.districtdemo_PercentAsianPacificIslander));
-    hispanic = Math.round(data.properties.districtdemo_PercentHispanic);
-    white = Math.round(data.properties.districtdemo_PercentWhite);
-    african = Math.round(data.properties.districtdemo_PercentBlack);
-    return "<div class='popupbox'><table>" + 
-                "<tr class='district-name row'><td>" + data.properties.NAME + "</td></tr>" +
-                "<tr class='lunch data row'><td class='percent'>" + Math.round(data.properties.districtdemo_freeredlunch) + "%" + "</td><td class='label'>% Free / reduced lunch</td></tr>" +
-                "<tr class='grad data row'><td class='percent'>" + Math.round(data.properties.districtdemo_GradRate) + "%" + "</td><td class='label'>Graduation rate</td></tr>" +
-                "<tr class='esl data row'><td class='percent'>" + Math.round(data.properties.districtdemo_ESL) + "%" + "</td><td class='label'>% ESL</td></tr>" +
-                "<tr class='graph row'>" + 
-                    "<td class='asian'>" +  
-                        "<div class='bar' style='height:" + (asian/100 * graphHeight) + "px;'><p>" + asian + "%</p></div>" +
-                        "<p class='tag'>asian</p>" +
-                    "</td>" +
-                    "<td class='hispanic'>" +  
-                        "<div class='bar' style='height:" + (hispanic/100 * graphHeight) + "px;'><p>" + hispanic + "%</p></div>" +
-                        "<p class='tag'>hispanic</p>" +
-                    "</td>" +
-                    "<td class='white'>" +  
-                        "<div class='bar' style='height:" + (white/100 * graphHeight) + "px;'><p>" + white + "%</p></div>" +
-                        "<p class='tag'>white</p>" +
-                    "</td>" +
-                    "<td class='african'>" +  
-                        "<div class='bar' style='height:" + (african/100 * graphHeight) + "px;'><p>" + african + "%</p></div>" +
-                        "<p class='tag'>african</p>" +
-                    "</td>" +
-                "</tr>" +
-                "</table></div>";
 }
+
